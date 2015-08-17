6518b9259b661e4d451575aa43a66280a32b474d
Dileshni Jayasinghe <dileshni@unata.com>
2015-08-16 22:45:36 -0400

    Update lights.py
    more comments

--- lights.py
+++ lights.py
@@ -9,15 +9,21 @@ parser.add_argument("-d", "--duration", type=int, default=30, dest="duration")
 
 args = parser.parse_args();
 
-# specifies that you are referring to the pins by the
-# number of the pin on board
+# specifies that you are referring to the pins 
+# by the number of the pin on board.
+# GPIO.BCM refers to pins by the Broadcom chip specific pin number
+# which are the numbers after "GPIO" on slide #6
 GPIO.setmode(GPIO.BOARD)
+# set up 'pin mode' as output on all three pins
 GPIO.setup(16, GPIO.OUT)
 GPIO.setup(18, GPIO.OUT)
 GPIO.setup(22, GPIO.OUT)
 
 def start():
+	# setting the digital output of a pin to True
+	# will drive it to 3.3V
 	GPIO.output(16, True)
+	# pin output is set to 0V
 	GPIO.output(18, False)
 	GPIO.output(22, False)
 
